5fe4e61691df6350630c3d06b1a5b99c494a98ba
manticorexl <zip3366@naver.com>
2026-01-09 16:40:41 +0900

    update 1/8 16:40

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
 
-for _ in range(T):
-
-    cols, rows, k = map(int, input().split())
-
-    grid = [[0 for _ in range(cols)] for _ in range(rows)]
-
-    for _ in range(k):
-        x, y = map(int, input().split())
-
-        grid[x][y] = 1
-
-    
-    for r in grid:
-        print(r)
