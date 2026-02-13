d0283c6e2e0927d5ffe4495d43a4f9947e429466
최민석 <130516868+ManticoreXL@users.noreply.github.com>
2026-02-13 10:36:56 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	queue<int> q;
+
+	for (int i = 1; i <= n; i++)
+	{
+		q.push(i);
+	}
+
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front() << endl;
+
+	return 0;
+}
