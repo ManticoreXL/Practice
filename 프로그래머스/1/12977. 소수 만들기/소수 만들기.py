330073c0742ec7a3c8d270093a4d0c1a7418bd30
최민석 <130516868+ManticoreXL@users.noreply.github.com>
2026-03-05 08:59:02 +0900

    [level 1] Title: 소수 만들기, Time: 16.04 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,23 @@
+import math
+from itertools import combinations
+
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+        
+    return True
+
+def solution(nums):
+    answer = 0
+    
+    for i in combinations(nums, 3):
+        if is_prime(sum(i)):
+            answer += 1
+
+    return answer
+
+# print(solution([1, 2, 7, 6, 4]))
